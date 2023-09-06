0b0cc64532efd69698371e7c50b9a72c9dbec8e4
Chonthanawat Kaewkan <01134k@Chonthanawats-MacBook-Pro.local>
2023-09-06 18:50:19 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import HomeScreen from './src/screens/HomeScreen';
+import APIScreen from './src/screens/APIScreens';
+const Stack = createNativeStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" 
+          component={HomeScreen}
+          options={{
+            title: "AOBUT",
+            headerTintColor:"#fff",
+            headerStyle: {
+            backgroundColor: '#1E4543',
+          }
+        }} />
+        <Stack.Screen name="API" 
+          component={APIScreen}
+          // options={{
+          //   title:"API APP",
+          //   headerTintColor:"#fff",
+          //   headerStyle:{
+          //     backgroundColor:'00594D',
+          //   }
+              
+          //   }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
